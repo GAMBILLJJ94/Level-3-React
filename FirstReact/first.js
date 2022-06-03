c0180fa99c5d2d4fe70220bf1b2f304c069ff273
Joseph Gambill <joey.gambill94@gmail.com>
2022-06-03 11:03:25 -0400

    added react folder with react import

--- FirstReact/first.js
+++ FirstReact/first.js
@@ -1,4 +1,24 @@
-ReactDOM.render(
-    <ul><li>Thing 7</li><li>Thing 9</li></ul>,
-    document.getElementById("root")
+function MainContent() {
+    return (
+        <h1>I'm learning React!</h1>
+    )
+}
+
+
+const navbar = (
+    <nav>
+        <h1>Joey's Portfolio of JS React Rendering</h1>
+        <h2>Below you can find pricing by the hour</h2>
+        <ul>
+            <li>New website build 50$</li>
+            <li>Website makeover 40$</li>
+            <li>Webiste advice 10$</li>
+        </ul>
+    </nav>
 )
+
+const root = document.getElementById("root")
+
+ReactDOM.render(navbar, root)
+
+// ReactDOM.render(<MainContent/>, root)
